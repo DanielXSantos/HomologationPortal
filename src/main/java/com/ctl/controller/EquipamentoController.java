d6ad973525c3b0cccd1811d48267804c1b06e48a
DanielSantos_ <daniel.victorst@gmail.com>
2018-02-27 11:13:22 -0300

    button fixed

--- src/main/java/com/ctl/controller/EquipamentoController.java
+++ src/main/java/com/ctl/controller/EquipamentoController.java
@@ -40,211 +40,214 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-@RequestMapping(value={"/equipamento"})
+@RequestMapping(value = {"/equipamento"})
 public class EquipamentoController {
-	
-	@Autowired
-	private EquipamentoRepository equipamentoRepository;
-	private FabricanteRepository fabricanteRepository;
-	private FeaturesRepository featuresRepository;
-	private PrecificacaoRepository precificacaoRepository;
-	private TipoRepository tipoRepository;
-        
-        private static String uploadDir = System.getProperty("user.dir")+"/uploads/";
-	
-	public EquipamentoController(EquipamentoRepository equipamentoRepository, FabricanteRepository fabricanteRepository, FeaturesRepository featuresRepository, PrecificacaoRepository precificacaoRepository, TipoRepository tipoRepository) {
-		this.equipamentoRepository = equipamentoRepository;
-		this.fabricanteRepository = fabricanteRepository;
-		this.featuresRepository = featuresRepository;
-		this.precificacaoRepository = precificacaoRepository;
-		this.tipoRepository = tipoRepository;
-	}
-	
-	@GetMapping
-	public String list(Model model) {
-		model.addAttribute("equipamento", new Equipamento());
-		model.addAttribute("equipamentos", equipamentoRepository.findAll());
-		return "equipamento/listar";
-	}
-	
-	@GetMapping("/editar")
-	public String edit(Model model, @RequestParam Long id) {
-		model.addAttribute("equipamento", equipamentoRepository.findOne(id));
-		model.addAttribute("fabricantes", fabricanteRepository.findAll());
-		model.addAttribute("featuress", featuresRepository.findAll());
-		model.addAttribute("precificacaos", precificacaoRepository.findAll());
-		model.addAttribute("tipos", tipoRepository.findAll());
-		return "equipamento/formulario";
-	}
-	
-	@GetMapping("/view")
-	public String view(Model model, @RequestParam Long id) {
-                String[] anexos  = new File(uploadDir+id.toString()+"/anexos").list();
-                model.addAttribute("anexos", anexos);
-		model.addAttribute("equipamento", equipamentoRepository.findOne(id));
-		model.addAttribute("fabricantes", fabricanteRepository.findAll());
-		model.addAttribute("featuress", featuresRepository.findAll());
-		model.addAttribute("precificacaos", precificacaoRepository.findAll());
-		model.addAttribute("tipos", tipoRepository.findAll());
-                
-		return "equipamento/descricao";
-	}
-	
-	
-	@GetMapping("/novo")
-	public String novo(Model model) {
-		model.addAttribute("equipamento", new FormEquipamento());
-		model.addAttribute("fabricantes", fabricanteRepository.findAll());
-		model.addAttribute("featuress", featuresRepository.findAll());
-		model.addAttribute("precificacaos", precificacaoRepository.findAll());
-		model.addAttribute("tipos", tipoRepository.findAll());
-		return "equipamento/formulario";
-	}
-	
-	@PostMapping("/salvar")
-	public String salvar(@Valid @ModelAttribute("equipamento") FormEquipamento equipamento,
-                            BindingResult bindingResult, Model model) {
-		if(bindingResult.hasErrors()) {
-                        model.addAttribute("equipamento", new FormEquipamento());
-			model.addAttribute("fabricantes", fabricanteRepository.findAll());
-			model.addAttribute("featuress", featuresRepository.findAll());
-			model.addAttribute("precificacaos", precificacaoRepository.findAll());
-			model.addAttribute("tipos", tipoRepository.findAll());
-			return "equipamento/formulario";
-		} 
-                
-                try{
-                    equipamento.setEquipamento(equipamentoRepository.save(equipamento.getEquipamento()));
-                    String path = uploadDir + equipamento.getEquipamento().getId();
-                    // Abrindo a pasta, e criando se não existe
-                    File f = new File(path);
-                    if(!f.exists())
-                        f.mkdirs();
-                    
-                    // salvando imagem
-                    String imgType = equipamento.getImagem().getOriginalFilename();
-                    int pointer = imgType.lastIndexOf(".");
-                    if(pointer == -1)
-                        throw new Exception("Imagem não inserida");
-                    imgType = imgType.substring(pointer);
-                    File img = new File(path + "/image");// + imgType);
-                    img.createNewFile();
-                    equipamento.getImagem().transferTo(img);
-                    
-                    // verifica se o usuário inseriu o caderno e o dataSheet
-                    // todo: validar formulário
-                    if(equipamento.getCaderno().getOriginalFilename().isEmpty() &&
-                            equipamento.getDataSheet().getOriginalFilename().isEmpty())
-                        throw new Exception("Caderno e DataSheet é obrigatorio");
-                    
-                    // salvando caderno de Testes
-                    File caderno = new File(path+"/caderno.pdf");
-                    caderno.createNewFile();
-                    equipamento.getCaderno().transferTo(caderno);
-                    
-                    // salvando dataSheet
-                    File dataSheet = new File(path+"/dataSheet.pdf");
-                    dataSheet.createNewFile();
-                    equipamento.getDataSheet().transferTo(dataSheet);
-                    
-                    // Salvando anexos
-                    if(equipamento.getFiles().length > 0){
-                        new File(path+"/anexos").mkdirs();
-                        for(MultipartFile file: equipamento.getFiles()){
-                            file.transferTo(new File(path+"/anexos/"+file.getOriginalFilename()));
-                        }
-                    }
-                    
-                }catch(Exception e){
-                    File directory = new File(uploadDir+equipamento.getEquipamento().getId());
-                    try{
-                        FileUtils.cleanDirectory(directory);
-                        FileUtils.forceDelete(directory);
-                    }catch(Exception ex){}
-                    equipamentoRepository.delete(equipamento.getEquipamento());
-                    e.printStackTrace();
-                    model.addAttribute("equipamento", new FormEquipamento());
-                    model.addAttribute("fabricantes", fabricanteRepository.findAll());
-                    model.addAttribute("featuress", featuresRepository.findAll());
-                    model.addAttribute("precificacaos", precificacaoRepository.findAll());
-                    model.addAttribute("tipos", tipoRepository.findAll());
-                    return "equipamento/formulario";
+
+    @Autowired
+    private EquipamentoRepository equipamentoRepository;
+    private FabricanteRepository fabricanteRepository;
+    private FeaturesRepository featuresRepository;
+    private PrecificacaoRepository precificacaoRepository;
+    private TipoRepository tipoRepository;
+
+    private static String uploadDir = System.getProperty("user.dir") + "/uploads/";
+
+    public EquipamentoController(EquipamentoRepository equipamentoRepository, FabricanteRepository fabricanteRepository, FeaturesRepository featuresRepository, PrecificacaoRepository precificacaoRepository, TipoRepository tipoRepository) {
+        this.equipamentoRepository = equipamentoRepository;
+        this.fabricanteRepository = fabricanteRepository;
+        this.featuresRepository = featuresRepository;
+        this.precificacaoRepository = precificacaoRepository;
+        this.tipoRepository = tipoRepository;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("equipamento", new Equipamento());
+        model.addAttribute("equipamentos", equipamentoRepository.findAll());
+        return "equipamento/listar";
+    }
+
+    @GetMapping("/editar")
+    public String edit(Model model, @RequestParam Long id) {
+        model.addAttribute("equipamento", equipamentoRepository.findOne(id));
+        model.addAttribute("fabricantes", fabricanteRepository.findAll());
+        model.addAttribute("featuress", featuresRepository.findAll());
+        model.addAttribute("precificacaos", precificacaoRepository.findAll());
+        model.addAttribute("tipos", tipoRepository.findAll());
+        return "equipamento/formulario";
+    }
+
+    @GetMapping("/view")
+    public String view(Model model, @RequestParam Long id) {
+        String[] anexos = new File(uploadDir + id.toString() + "/anexos").list();
+        model.addAttribute("anexos", anexos);
+        model.addAttribute("equipamento", equipamentoRepository.findOne(id));
+        model.addAttribute("fabricantes", fabricanteRepository.findAll());
+        model.addAttribute("featuress", featuresRepository.findAll());
+        model.addAttribute("precificacaos", precificacaoRepository.findAll());
+        model.addAttribute("tipos", tipoRepository.findAll());
+
+        return "equipamento/descricao";
+    }
+
+    @GetMapping("/novo")
+    public String novo(Model model) {
+        model.addAttribute("equipamento", new FormEquipamento());
+        model.addAttribute("fabricantes", fabricanteRepository.findAll());
+        model.addAttribute("featuress", featuresRepository.findAll());
+        model.addAttribute("precificacaos", precificacaoRepository.findAll());
+        model.addAttribute("tipos", tipoRepository.findAll());
+        return "equipamento/formulario";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@Valid @ModelAttribute("equipamento") FormEquipamento equipamento,
+            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("equipamento", new FormEquipamento());
+            model.addAttribute("fabricantes", fabricanteRepository.findAll());
+            model.addAttribute("featuress", featuresRepository.findAll());
+            model.addAttribute("precificacaos", precificacaoRepository.findAll());
+            model.addAttribute("tipos", tipoRepository.findAll());
+            return "equipamento/formulario";
+        }
+
+        try {
+            equipamento.setEquipamento(equipamentoRepository.save(equipamento.getEquipamento()));
+            String path = uploadDir + equipamento.getEquipamento().getId();
+            // Abrindo a pasta, e criando se não existe
+            File f = new File(path);
+            if (!f.exists()) {
+                f.mkdirs();
+            }
+
+            // salvando imagem
+            String imgType = equipamento.getImagem().getOriginalFilename();
+            int pointer = imgType.lastIndexOf(".");
+            if (pointer == -1) {
+                throw new Exception("Imagem não inserida");
+            }
+            imgType = imgType.substring(pointer);
+            File img = new File(path + "/image");// + imgType);
+            img.createNewFile();
+            equipamento.getImagem().transferTo(img);
+
+            // verifica se o usuário inseriu o caderno e o dataSheet
+            // todo: validar formulário
+            if (equipamento.getCaderno().getOriginalFilename().isEmpty()
+                    && equipamento.getDataSheet().getOriginalFilename().isEmpty()) {
+                throw new Exception("Caderno e DataSheet é obrigatorio");
+            }
+
+            // salvando caderno de Testes
+            File caderno = new File(path + "/caderno.pdf");
+            caderno.createNewFile();
+            equipamento.getCaderno().transferTo(caderno);
+
+            // salvando dataSheet
+            File dataSheet = new File(path + "/dataSheet.pdf");
+            dataSheet.createNewFile();
+            equipamento.getDataSheet().transferTo(dataSheet);
+
+            // Salvando anexos
+            if (equipamento.getFiles().length > 0) {
+                new File(path + "/anexos").mkdirs();
+                for (MultipartFile file : equipamento.getFiles()) {
+                    file.transferTo(new File(path + "/anexos/" + file.getOriginalFilename()));
+                }
+            }
+
+        } catch (Exception e) {
+            File directory = new File(uploadDir + equipamento.getEquipamento().getId());
+            try {
+                FileUtils.cleanDirectory(directory);
+                FileUtils.forceDelete(directory);
+            } catch (Exception ex) {
+            }
+            equipamentoRepository.delete(equipamento.getEquipamento());
+            e.printStackTrace();
+            model.addAttribute("equipamento", new FormEquipamento());
+            model.addAttribute("fabricantes", fabricanteRepository.findAll());
+            model.addAttribute("featuress", featuresRepository.findAll());
+            model.addAttribute("precificacaos", precificacaoRepository.findAll());
+            model.addAttribute("tipos", tipoRepository.findAll());
+            return "equipamento/formulario";
 //                    return "equipamento/listar";
 
-                }
+        }
 //                return "redirect:/"; 
-		return "redirect:/equipamento";
-	}
-	
-	@GetMapping("/excluir")
-	public String excluir(Model model, @RequestParam Long id) {
-		equipamentoRepository.delete(id);
-		return "redirect:/"; 
-	}
-	
-	@GetMapping("/buscar")
-	public String buscar(Model model, @RequestParam String nome) {
-		model.addAttribute("equipamento", new Equipamento());
-		model.addAttribute("equipamentos", equipamentoRepository.findByNomeLike("%" + nome + "%"));
-		return "equipamento/formulario";
-	}
-        
-        @GetMapping("/image/{imageName}")
-        @ResponseBody
-        public byte[] getImage(@PathVariable("imageName") Integer fileName){
-            try{
-                File file = new File(uploadDir+fileName+"/image");
-                return Files.readAllBytes(file.toPath());
-            } catch (IOException ex) {
-                Logger.getLogger(EquipamentoController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return null;
-        }
-        
-        @GetMapping("/pdf/{type}/{imageName}")
-        @ResponseBody
-        public ResponseEntity<byte[]> getPDF(@PathVariable("imageName") Integer fileName, @PathVariable("type") Integer type){
-            try{
-                File file;
-                if(type==1){
-                    file = new File(uploadDir+fileName+"/dataSheet.pdf");
-                }else{
-                    file = new File(uploadDir+fileName+"/caderno.pdf");
-                }
-                byte[] contents = Files.readAllBytes(file.toPath());
-                HttpHeaders headers = new HttpHeaders();
-                headers.setContentType(MediaType.parseMediaType("application/pdf"));
-                String filename = "output.pdf";
-                headers.setContentDispositionFormData(filename, filename);
-                headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-                ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
-                return response;
-            } catch (IOException ex) {
-                Logger.getLogger(EquipamentoController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            return null;
-        }
-        
-        @GetMapping("/file/{id}/{fileName}.{type}")
-        public void downloadFile(HttpServletResponse resonse, @PathVariable(value="id") Integer id, @PathVariable(value="fileName") String fileName,
-                                @PathVariable(value="type") String tipo) {
-            try {
-                File file = new File(uploadDir+id.toString()+"/anexos/"+fileName + "." + tipo);
-                resonse.setContentType("application/pdf");
-                resonse.setHeader("Content-Disposition", "attachment;filename=" + fileName + "."+ tipo);
-                BufferedInputStream inStrem = new BufferedInputStream(new FileInputStream(file));
-                BufferedOutputStream outStream = new BufferedOutputStream(resonse.getOutputStream());
+        return "redirect:/equipamento";
+    }
 
-                byte[] buffer = new byte[1024];
-                int bytesRead = 0;
-                while ((bytesRead = inStrem.read(buffer)) != -1) {
-                  outStream.write(buffer, 0, bytesRead);
-                }
-                outStream.flush();
-                inStrem.close();
-            } catch (IOException ex) {
-                Logger.getLogger(EquipamentoController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+    @GetMapping("/excluir")
+    public String excluir(Model model, @RequestParam Long id) {
+        equipamentoRepository.delete(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/buscar")
+    public String buscar(Model model, @RequestParam String nome) {
+        model.addAttribute("equipamento", new Equipamento());
+        model.addAttribute("equipamentos", equipamentoRepository.findByNomeLike("%" + nome + "%"));
+        return "equipamento/formulario";
+    }
+
+    @GetMapping("/image/{imageName}")
+    @ResponseBody
+    public byte[] getImage(@PathVariable("imageName") Integer fileName) {
+        try {
+            File file = new File(uploadDir + fileName + "/image");
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException ex) {
+            Logger.getLogger(EquipamentoController.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return null;
+    }
+
+    @GetMapping("/pdf/{type}/{imageName}")
+    @ResponseBody
+    public ResponseEntity<byte[]> getPDF(@PathVariable("imageName") Integer fileName, @PathVariable("type") Integer type) {
+        try {
+            File file;
+            if (type == 1) {
+                file = new File(uploadDir + fileName + "/dataSheet.pdf");
+            } else {
+                file = new File(uploadDir + fileName + "/caderno.pdf");
+            }
+            byte[] contents = Files.readAllBytes(file.toPath());
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType("application/pdf"));
+            String filename = "output.pdf";
+            headers.setContentDispositionFormData(filename, filename);
+            headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+            ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
+            return response;
+        } catch (IOException ex) {
+            Logger.getLogger(EquipamentoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    @GetMapping("/file/{id}/{fileName}.{type}")
+    public void downloadFile(HttpServletResponse resonse, @PathVariable(value = "id") Integer id, @PathVariable(value = "fileName") String fileName,
+            @PathVariable(value = "type") String tipo) {
+        try {
+            File file = new File(uploadDir + id.toString() + "/anexos/" + fileName + "." + tipo);
+            resonse.setContentType("application/pdf");
+            resonse.setHeader("Content-Disposition", "attachment;filename=" + fileName + "." + tipo);
+            BufferedInputStream inStrem = new BufferedInputStream(new FileInputStream(file));
+            BufferedOutputStream outStream = new BufferedOutputStream(resonse.getOutputStream());
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = 0;
+            while ((bytesRead = inStrem.read(buffer)) != -1) {
+                outStream.write(buffer, 0, bytesRead);
+            }
+            outStream.flush();
+            inStrem.close();
+        } catch (IOException ex) {
+            Logger.getLogger(EquipamentoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
