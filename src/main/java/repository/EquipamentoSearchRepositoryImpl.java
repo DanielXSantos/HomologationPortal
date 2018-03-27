5753008d5a5d99e2ebd5833cdcc02ea799b0b3cf
root <root@localhost.localdomain>
2018-03-27 20:16:17 -0300

    Advanced Search Complete without search for date

--- src/main/java/repository/EquipamentoSearchRepositoryImpl.java
+++ src/main/java/repository/EquipamentoSearchRepositoryImpl.java
@@ -8,13 +8,17 @@ package com.ctl.repository;
 import com.ctl.model.Equipamento;
 import com.ctl.model.Features;
 import form.SearchForm;
+import java.util.Collection;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.SetJoin;
 import javax.persistence.criteria.Subquery;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,7 +43,7 @@ public class EquipamentoSearchRepositoryImpl implements EquipamentoSearchReposit
         Predicate predicate = builder.conjunction();
         
         if(s.getNome() != null && !s.getNome().isEmpty()){
-            predicate = builder.and(builder.like(r.get("nome"),"%"+s.getNome()+"%"));
+            predicate = builder.and(predicate, builder.like(r.get("nome"),"%"+s.getNome()+"%"));
         }
         if(s.getStatus() != null && !s.getStatus().isEmpty()){
             predicate = builder.and(predicate, r.get("status").in(s.getStatus()));
@@ -54,13 +58,13 @@ public class EquipamentoSearchRepositoryImpl implements EquipamentoSearchReposit
             predicate = builder.and(predicate, r.get("tipo").in(s.getTipos()));
         }
         if(s.getFeatures() != null && !s.getFeatures().isEmpty()){
-//            Subquery<Features> sub = query.subquery(Features.class);
-//            Root<Features> subR = sub.from(Features.class);
-//            
-//            sub.where(builder.equal(r.join("features"), s.getFeatures()));
-//            predicate = builder.and(predicate, builder.in(sub));
+            predicate = builder.and(predicate, r.join("features").in(s.getFeatures()));
+        }
+        if(s.getHomologado() != null && !s.getHomologado().isEmpty()){
+            predicate = builder.and(predicate, r.join("homologado").in(s.getHomologado()));
         }
 
+        query.distinct(true);
         query.where(predicate);
         return em.createQuery(query).getResultList();
     }
