a9b3ffbd05186d69a7259140402872bfe135e064
DanielSantos_ <daniel.victorst@gmail.com>
2018-04-01 08:59:00 -0300

    Adding requisitos

---
+++ src/main/java/controller/RequisitoController.java
@@ -0,0 +1,81 @@
+package com.ctl.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ctl.model.Equipamento;
+import com.ctl.model.Requisito;
+import com.ctl.repository.RequisitoRepository;
+import com.ctl.util.AdvancedSearchUtil;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+@RequestMapping("/requisito")
+public class RequisitoController {
+
+    @Autowired
+    private RequisitoRepository requisitoRepository;
+    
+    @Autowired
+    private AdvancedSearchUtil advancedSearch;
+    
+    @GetMapping
+    public String list(Model model) {
+        System.out.println("What");
+        model = advancedSearch.build(model);
+        model.addAttribute("requisitos", requisitoRepository.findAll());
+        List hello = (List) requisitoRepository.findAll();
+        System.out.println(hello.get(0));
+        System.out.println("que");
+        return "requisito/listar";
+    }
+
+    @GetMapping("/editar")
+    public String edit(Model model, @RequestParam Long id) {
+        model = advancedSearch.build(model);
+        model.addAttribute("requisito", requisitoRepository.findOne(id));
+        return "requisito/formulario";
+    }
+
+    @GetMapping("/view")
+    public String view(Model model, @RequestParam Long id) {
+        model = advancedSearch.build(model);
+        model.addAttribute("requisito", requisitoRepository.findOne(id));
+        return "requisito/descricao";
+    }
+
+    @GetMapping("/novo")
+    public String novo(Model model) {
+        model = advancedSearch.build(model);
+        model.addAttribute("requisito", new Requisito());
+        return "requisito/formulario";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(@Valid Requisito requisito, BindingResult bindingResult, Model model) {
+        model = advancedSearch.build(model);
+        if (bindingResult.hasErrors()) {
+            return "requisito/formulario";
+        }
+        requisitoRepository.save(requisito);
+        return "redirect:/requisito/";
+
+    }
+
+    @GetMapping("/buscar")
+    public String buscar(Model model, @RequestParam String nome) {
+        model = advancedSearch.build(model);
+        model.addAttribute("requisito", new Equipamento());
+        model.addAttribute("requisitos", requisitoRepository.findByNomeLike("%" + nome + "%"));
+        return "requisito/listar";
+    }
+
+}
---
+++ src/main/java/repository/RequisitoRepository.java
@@ -0,0 +1,11 @@
+package com.ctl.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ctl.model.Requisito;
+
+public interface RequisitoRepository extends CrudRepository<Requisito, Long>{
+	List<Requisito> findByNomeLike(String nome);
+}
