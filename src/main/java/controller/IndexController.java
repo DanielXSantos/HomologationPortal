de624dc6916cbefd21ec491db590b67275858bca
DanielSantos_ <daniel.victorst@gmail.com>
2018-04-02 17:05:47 -0300

    changing descricao requisitos

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author daniel
+ */
+
+@Controller
+@RequestMapping("/main")
+public class IndexController {
+
+    @GetMapping
+    public String home() {
+        return "layout/main";
+    }
+}
