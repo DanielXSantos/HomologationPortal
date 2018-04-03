18367e09d7b327e5c0c2f6dace580f34380ae17e
Lucas <lucasgomezsousa@gmail.com>
2018-04-03 13:49:01 -0300

    Criacao Historia e Politica Homologacao

---
+++ src/main/java/controller/HistoriaController.java
@@ -0,0 +1,31 @@
+package com.ctl.controller;
+
+import model.Historia;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ctl.util.AdvancedSearchUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+@RequestMapping(value = "/historia")
+public class HistoriaController {
+
+    @Autowired
+    private AdvancedSearchUtil search;
+
+    @GetMapping
+    public String list(Model model) {
+        model = search.build(model);
+        model.addAttribute("historia", new Historia());
+
+        return "historia/texto";
+    }
+}
