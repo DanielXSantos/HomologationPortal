d488620e54efd67c2ce31ec310d689850fe37236
Ronistone Junior <ronistonejunior@gmail.com>
2018-04-06 14:54:26 -0300

    update

--- src/main/resources/db/migration/V2__create_tables.sql
+++ src/main/resources/db/migration/V2__create_tables.sql
@@ -1,2 +1,17 @@
--- INSERT INTO `role` VALUES (1,'ADMIN');
--- INSERT INTO `role` VALUES (2,'USER');
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(255) DEFAULT NULL,
+  `users_user_id` int(11),
+  PRIMARY KEY (`role_id`)
+);
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `active` int(11) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `expiration_date` date NOT NULL,
+  `fabricante_id` BIGINT,
+  PRIMARY KEY (`user_id`),
+  FOREIGN KEY (`fabricante_id`) REFERENCES `fabricante`(`id`)
+);
